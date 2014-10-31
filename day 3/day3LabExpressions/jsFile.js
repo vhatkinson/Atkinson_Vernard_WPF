b254eb580796c3393fe4479dedc4ac1c07f9f8e5
vhatkinson <vernard12345@GMail.com>
2014-10-31 17:26:43 -0400

    slices per person
    including decimal

--- day 3/day3LabExpressions/jsFile.js
+++ day 3/day3LabExpressions/jsFile.js
@@ -2,10 +2,17 @@
  * Created by vernard on 10/31/14.
  */
 //Vernard Atkinson, oct. 31, day #3, WPF, Expressions
+
 //number of slices per pizza
 var slices= 8;
 //number of people at party
-var people= 14
+var people= 14;
 //number of pizzas ordered
-var pizzas=2
+var pizzas=2;
+var slicesPP= ((slices*pizzas)/people);
+console.log ("the number of slices each person would eat would be"+" "+slicesPP);
+
+
+
+
 
