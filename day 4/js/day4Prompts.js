930b580af7238f57f5ca12dc3a55562047a0211f
vhatkinson <vernard12345@GMail.com>
2014-11-03 18:51:17 -0500

    Calculated age
    based on year prompts

---
+++ day 4/js/day4Prompts.js
@@ -0,0 +1,16 @@
+/**
+ * Vernard Atkinson
+ * day 4 prompts
+ * WPF 1411
+ * 11/3/14.
+ */
+//alert("testing 1,2,3");
+
+//ask user for input
+//prompt()- asks user to input information
+var yearBorn=prompt("Enter your year of birth");
+console.log(yearBorn);
+
+//figure out how old we are
+var age= 2014-yearBorn;
+console.log("you are "+age+" years old");
