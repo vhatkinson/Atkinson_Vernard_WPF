dc80abfb2be9294f9b27c5987f4a65997fa470f0
vhatkinson <vernard12345@GMail.com>
2014-11-04 00:18:03 -0500

    created an array
    array about information asked

--- expressions/expressions_Industry/js/industry.js
+++ expressions/expressions_Industry/js/industry.js
@@ -6,7 +6,17 @@
  */
 //alert("testing 1,2,3");
 
-//How much money will you make in a year.
+//How much money will you make in a year
+//information about yourself
+
+//how much money is made per hour.
 var salary=prompt("We will be calculating how much spending money you will have per year.\n How much do you make per hour?");
+//How many hours per month do you work.
+var hoursMonthly=prompt("How many hours do you work per month?");
 //how much money spent on cost of living
-var monthlyExpenses=("how much money do you spend a month on bills, gas, and food?");
+var monthlyExpenses=prompt("how much money do you spend a month on bills, gas, and food?");
+
+var personalInfo=[salary,hoursMonthly,monthlyExpenses];
+
+console.log(personalInfo);
+
