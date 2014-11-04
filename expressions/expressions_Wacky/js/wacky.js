75400448f5b387b942a817a5cc55bb64127c153d
vhatkinson <vernard12345@GMail.com>
2014-11-04 01:24:49 -0500

    rap name alert
    added alert to let user know what purpose of site is.

--- expressions/expressions_Wacky/js/wacky.js
+++ expressions/expressions_Wacky/js/wacky.js
@@ -7,8 +7,10 @@
 //alert("testing 1,2,3");
 
 //create your own rap name
+alert("We are going create your own rap name.")
 var color=prompt("what is your favorite color?");
 var verb=prompt("what is your favorite activity");
 var animal=prompt("What is your favorite animal?");
 var rapperName=("your rapper name is:\n"+color+" "+verb+" "+animal);
+console.log(rapperName);
 
