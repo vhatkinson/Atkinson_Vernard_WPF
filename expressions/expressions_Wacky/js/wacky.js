81a1e54fc0db8a7961898fee0a000ad420e2061c
vhatkinson <vernard12345@GMail.com>
2014-11-04 01:19:45 -0500

    creating rap name
    creating rap name

--- expressions/expressions_Wacky/js/wacky.js
+++ expressions/expressions_Wacky/js/wacky.js
@@ -9,5 +9,6 @@
 //create your own rap name
 var color=prompt("what is your favorite color?");
 var verb=prompt("what is your favorite activity");
-
+var animal=prompt("What is your favorite animal?");
+var rapperName=("your rapper name is:\n"+color+" "+verb+" "+animal);
 
