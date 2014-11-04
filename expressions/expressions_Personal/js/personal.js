6e0a01ae7f58adcf0d30bbafa6be6059015aae7c
vhatkinson <vernard12345@GMail.com>
2014-11-03 22:28:32 -0500

    added price of stick
    prompted them to enter how much the stick costs.

--- expressions/expressions_Personal/js/personal.js
+++ expressions/expressions_Personal/js/personal.js
@@ -4,4 +4,7 @@
  * WPF 1411
  * Nov. 3, 2014
  */
-//alert("testing 1,2,3");
+//alert("testing 1,2,3")
+
+//calculating the cost of a new lacrosse stick with tax.
+var stick=prompt("We are calculating the price of your new lacrosse stick and head including tax.\nEnter the price of the lacrosse stick.");
