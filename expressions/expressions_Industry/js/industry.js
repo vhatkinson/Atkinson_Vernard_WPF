d2d1ff769e8588cc3b59b9e79f531cf1f4b3da45
vhatkinson <vernard12345@GMail.com>
2014-11-04 00:02:53 -0500

    cost of living
    prompt to input cost of living

--- expressions/expressions_Industry/js/industry.js
+++ expressions/expressions_Industry/js/industry.js
@@ -8,3 +8,5 @@
 
 //How much money will you make in a year.
 var salary=prompt("We will be calculating how much spending money you will have per year.\n How much do you make per hour?");
+//how much money spent on cost of living
+var monthlyExpenses=("how much money do you spend a month on bills, gas, and food?");
