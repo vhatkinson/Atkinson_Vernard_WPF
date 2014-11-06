177eae199fceaa337ce49bf818a36b18c6500071
vhatkinson <vernard12345@GMail.com>
2014-11-05 23:17:01 -0500

    c & f to if then
    added variable to ask for c or f

--- conditionalsWorksheet/js/celsiusToFahrenheitConverter.js
+++ conditionalsWorksheet/js/celsiusToFahrenheitConverter.js
@@ -8,6 +8,9 @@
 //converting celsius to fahrenheit
 //°C  x  9/5 + 32 = °F
 //(°F  -  32)  x  5/9 = °C
+var temp=prompt("enter c if converting Celsius to Fahrenheit. \n enter f if converting Fahrenheit to Celsius.");
+
+
 var celcius=prompt("enter the degrees in Celsius to find out what it is in Fahrenheit.");
 var fahrenheit=celcius*(9/5)+32;
 console.log(fahrenheit);
