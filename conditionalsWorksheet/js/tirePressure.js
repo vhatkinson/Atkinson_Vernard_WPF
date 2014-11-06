f52727e0bf3620747a6f9e9a495c8505acbc9095
vhatkinson <vernard12345@GMail.com>
2014-11-06 01:23:37 -0500

    tire pressure [0]
    front left tire pressure given value for array

---
+++ conditionalsWorksheet/js/tirePressure.js
@@ -0,0 +1,12 @@
+/**
+ * Vernard Atkinson
+ * Tire Pressure
+ * WPF 1411
+ * Nov. 6, 2014
+ */
+//alert("testing 1,2,3");
+
+//front tire pressure must mach rear tire pressure
+var tirePressure=[];
+tirePressure[0]=prompt("what is your front left tire pressure?");
+
