c4a656e0ff4d111c23307782220535d9d77a4c16
vhatkinson <vernard12345@GMail.com>
2014-11-06 17:37:19 -0500

    fixed grammatical errors
    added commas where they are needed.

--- conditionalsWorksheet/js/gradeLetterCalculator.js
+++ conditionalsWorksheet/js/gradeLetterCalculator.js
@@ -10,10 +10,10 @@
 var grade=prompt("We are going to find out your letter grade. \n What is your percentage grade?");
 grade=(grade<0)?"enter an actual grade.":
     (grade<=69)? "You have an F, Study Harder!" :
-        (grade<=72)? "You have a D you can do better!":
+        (grade<=72)? "You have a D, you can do better!":
             (grade<=79)?"You have a C, try harder!":
-                (grade<=89)?"You have a B that's pretty good.":
-                    (grade<=100)?"You have an A Great job!":
+                (grade<=89)?"You have a B, that's pretty good.":
+                    (grade<=100)?"You have an A, Great job!":
                         "enter a real number";
 console.log(grade);
 alert(grade);
