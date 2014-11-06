4b2b76f4b982fb6f30d13c41442fe17cd140fc74
vhatkinson <vernard12345@GMail.com>
2014-11-06 02:15:58 -0500

    true false results.
    if true tires pass test
    if false tires don’t pass test

--- conditionalsWorksheet/js/tirePressure.js
+++ conditionalsWorksheet/js/tirePressure.js
@@ -13,4 +13,10 @@ tirePressure[1]=prompt("what is your front right tire pressure?");
 tirePressure[2]=prompt("what is your back left tire pressure?");
 tirePressure[3]=prompt("what is your back right tire pressure?");
 console.log(tirePressure);
-console.log((tirePressure[0]==tirePressure[1]) && (tirePressure[2]==tirePressure[3]));
+var results=((tirePressure[0]==tirePressure[1]) && (tirePressure[2]==tirePressure[3]));
+console.log(tirePressure);
+if (results==true){
+    alert("Tires pass the specs!")
+}else if (results==false){
+    alert("Tires don't pass spec, get them checked out.")
+}
