3da5b2c3dd317240cbf9857e471fa083e3145ce9
vhatkinson <vernard12345@GMail.com>
2014-11-05 20:39:51 -0500

    created operator file
    finished steak also

--- day 5/js/day5Conditionals.js
+++ day 5/js/day5Conditionals.js
@@ -31,17 +31,26 @@ console.log("After the if statement");
 //if the kid is over 48 inches tall then he can ride the ride.
 
 //set the kids height
-var kidHeight=47;
+var kidHeight=44.9;
 
 //add a sneaker lift
 var sneakerLift=2;
 
+//create variable for kid minimum hight.
+var minHight=48;
+
+//if your parent goes on the rde with you you can be 45" and over to ride
+var wParentHight=45;
+
 //create if statement
-if(kidHeight>48){
+if(kidHeight>minHight){
     //true code goes here
     console.log("congrats your tall enough to ride");
+}else if(kidHeight>=wParentHight){
+    console.log("You can ride only with your parent.");
+
 }else if(kidHeight+sneakerLift>48){
-    console.log("You can ride the ride, you cheater.");
+    console.log("You can ride the ride, if you wear sneaker lift.");
 }else{
     //false code goes here
     console.log("sorry kid you're too short.");
