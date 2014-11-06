cfa240d99f247067941f9e9f365a493b97c8189f
vhatkinson <vernard12345@GMail.com>
2014-11-05 23:11:13 -0500

    finished celcius side of coding
    as above so below

--- conditionalsWorksheet/js/celsiusToFahrenheitConverter.js
+++ conditionalsWorksheet/js/celsiusToFahrenheitConverter.js
@@ -7,6 +7,8 @@
 //alert("testing 1,2,3");
 //converting celsius to fahrenheit
 //°C  x  9/5 + 32 = °F
+//(°F  -  32)  x  5/9 = °C
 var celcius=prompt("enter the degrees in Celsius to find out what it is in Fahrenheit.");
 var fahrenheit=celcius*(9/5)+32;
 console.log(fahrenheit);
+alert(celcius + " degrees Celcius is "+ fahrenheit +" degrees Fahrenheit.");
