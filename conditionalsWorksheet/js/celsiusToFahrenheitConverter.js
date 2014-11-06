7ae4ef51506370d0717b820dbe5b392ee921d2a8
vhatkinson <vernard12345@GMail.com>
2014-11-06 17:06:03 -0500

    Fixed spelling error
    celcius to celsius.

--- conditionalsWorksheet/js/celsiusToFahrenheitConverter.js
+++ conditionalsWorksheet/js/celsiusToFahrenheitConverter.js
@@ -12,12 +12,12 @@
 var temp=prompt("enter c if converting Celsius to Fahrenheit. \n enter f if converting Fahrenheit to Celsius.");
 if (temp==("c")){
     //calculating celsius to fahrenheit
-    var celcius = prompt("enter the degrees in Celsius to find out what it is in Fahrenheit.");
+    var celsius = prompt("enter the degrees in Celsius to find out what it is in Fahrenheit.");
 
     //c-f conversion rate
-    var celciusToFahrenheit = celcius * (9 / 5) + 32;
-    console.log(celciusToFahrenheit);
-    alert(celcius + " degrees Celsius is " + celciusToFahrenheit + " degrees Fahrenheit.");
+    var celsiusToFahrenheit = celsius * (9 / 5) + 32;
+    console.log(celsiusToFahrenheit);
+    alert(celsius + " degrees Celsius is " + celsiusToFahrenheit + " degrees Fahrenheit.");
 }else if (temp==("f")) {
     //calculating fahrenheit to celsius
     var fahrenheit=prompt("enter the degrees in fahrenheit to find out what it is in celsius.");
