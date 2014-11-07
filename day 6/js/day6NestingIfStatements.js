075274ac5f1ae4deb53270291a01e76d1ee3e856
vhatkinson <vernard12345@GMail.com>
2014-11-07 18:22:47 -0500

    nesting if statements finished
    this is solo cool

--- day 6/js/day6NestingIfStatements.js
+++ day 6/js/day6NestingIfStatements.js
@@ -6,26 +6,45 @@
  */
 //alert("testing 1,2,3");
 
-//some dicisions depend on other decisions.
+//some decisions depend on other decisions.
 //if it is warm enough let's go to the beach if not let's go to the movies.
 //if the water is warm enough let's go swimming if not get tan boyyy
+//choose between a kids movie and a horror movie(are you taking a child
 
 //get temp
-var temp=89;
+var temp=prompt("What's the temperature outside?");
 
 //water temp
-var waterTemp=80;
+var waterTemp=60;
+
+//are you taking a child
+var child=false;
 
 //if else statement
 if(temp>70){
     console.log("let's go to the beach!");
+    //What is the water temperature? it over-rides previous value
+    waterTemp=prompt("What's the temperature of the water?");
+
+
+    //test the water temp
+    if(waterTemp>75){
+        console.log("let's go swimming in the water!");
+    }else{
+        console.log("let us get tan!");
+    }
 }else{
     console.log("let's go to the movies!");
+    if(child){
+        var childAge=prompt("How old is the child?");
+        if(childAge<=6){
+            console.log("let's go see the smurf movie.");
+        }else{
+            console.log("let's go see indiana jones.");
+        }
+        console.log("go see a kids film.");
+    }else{
+        console.log("Go see a horror film.");
+    }
 }
 
-//test the water temp
-if(waterTemp>75){
-    console.log("let's go swimming in the water!");
-}else{
-    console.log("let us get tan!");
-}
