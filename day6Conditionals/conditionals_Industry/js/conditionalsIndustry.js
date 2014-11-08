c88e8dd2758d2527056d284990e68b63b096e338
vhatkinson <vernard12345@GMail.com>
2014-11-08 01:09:44 -0500

    added last name to array
    prompted user to input password

--- day6Conditionals/conditionals_Industry/js/conditionalsIndustry.js
+++ day6Conditionals/conditionals_Industry/js/conditionalsIndustry.js
@@ -14,3 +14,8 @@ var idInfo=[];
 //prompt for user first name
 idInfo[0]=prompt("To login to our website you mast create an account. (Don't capitalize letters) \n What is your first name?");
 
+//prompt for user to enter last name
+idInfo[1]=prompt("what is your last name");
+
+//prompt user to enter a password.
+idInfo[2]=prompt("Enter a password");
