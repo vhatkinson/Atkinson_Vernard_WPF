f2ff88661ff99d2367a132a6c478b605ec7a8558
vhatkinson <vernard12345@GMail.com>
2014-11-08 01:07:18 -0500

    added first name in array
    prompted user to put in first name

--- day6Conditionals/conditionals_Industry/js/conditionalsIndustry.js
+++ day6Conditionals/conditionals_Industry/js/conditionalsIndustry.js
@@ -11,3 +11,6 @@
 //created array for ID information.
 var idInfo=[];
 
+//prompt for user first name
+idInfo[0]=prompt("To login to our website you mast create an account. (Don't capitalize letters) \n What is your first name?");
+
