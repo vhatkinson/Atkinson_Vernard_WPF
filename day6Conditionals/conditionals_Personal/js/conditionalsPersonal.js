9b527d4358834d82120547148baf46d9aa018776
vhatkinson <vernard12345@GMail.com>
2014-11-08 00:03:34 -0500

    added if no answer
    didn’t loop it so only works once

--- day6Conditionals/conditionals_Personal/js/conditionalsPersonal.js
+++ day6Conditionals/conditionals_Personal/js/conditionalsPersonal.js
@@ -10,27 +10,39 @@
 alert("We are finding out if flying or driving will be a better mode of transportation. Assuming you have a car.");
 
 //finding the cost of a plane ticket
-var cost=prompt("How much is a plane ticket to your destination?");
-console.log(cost);
+var flyingCost=prompt("How much is a plane ticket to your destination?");
+if(flyingCost==""){
+    flyingCost=prompt("How much is a plane ticket to your destination?");
+}
+console.log(flyingCost);
 
 //what is the MPG of your vehicle?
 var mpg=prompt("What is the miles per gallon of your vehicle?");
+if(mpg==""){
+    mpg=("What is the miles per gallon of your vehicle?");
+}
 console.log(mpg);
 
 //how many miles are you from your destination?
 var distance=prompt("How many miles are you from your destination?");
+if (distance==""){
+    distance=("How many miles are you from your destination?");
+}
 console.log(distance);
 
 //what is the price of a gallon of gas?
 var gas=prompt("What is the price of a gallon of gas?");
+if (gas==""){
+    gas=("What is the price of a gallon of gas?");
+}
 console.log(gas);
 
 var drivingCost=(distance/mpg)*(gas);
 console.log(drivingCost);
 
-if (cost>drivingCost){
+if (flyingCost<drivingCost){
     console.log("It would be cheaper to fly.");
-}else if (cost==drivingCost){
+}else if (flyingCost==drivingCost){
     console.log("It cost the same to drive or fly so you get to choose.");
 }else{
     console.log("It would be cheaper to drive.");
