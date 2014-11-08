e259747535eadcd9c6dbb109beed36c2ca4b2b76
vhatkinson <vernard12345@GMail.com>
2014-11-08 02:05:10 -0500

    finished loops on industry
    have if, else if, and else and while.

--- day6Conditionals/conditionals_Industry/js/conditionalsIndustry.js
+++ day6Conditionals/conditionals_Industry/js/conditionalsIndustry.js
@@ -36,6 +36,23 @@ var idInfo2=[];
 // asking them for username.
 idInfo2[0]=prompt("What is your username?");
 
+//if they don't put anything it will make them do this until they do
+if(idInfo2[0]==""){
+    idInfo2[0]=prompt("You must enter a username.");
+}else if (idInfo[0]==idInfo2[0]){//this is if the username is the same so they just continue.
+
+}else{//this is if username is wrong.
+    idInfo2[0]=prompt("You must enter the correct username in our system.");
+}
+
 //asking them for password.
 idInfo2[1]=prompt("What is your password");
 
+//if they don't put anything it will make them do this until they do
+if(idInfo[1]=="") {
+    idInfo[1] = prompt("You must Enter a password.");
+}else if (idInfo[1]==idInfo2[1]){//this is if the password is the same.
+    alert("Congratulations, you can enter the site.")
+}else{//this is incase password is wrong
+    idInfo2[0]=prompt("You must enter the correct password in our system.");
+}  
