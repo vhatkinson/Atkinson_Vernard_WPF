294853fa72667518875e329957adc9356eb8937f
vhatkinson <vernard12345@GMail.com>
2014-11-07 22:41:39 -0500

    best mode of transportation.
    chose for choosing best way to travel

--- day6Conditionals/conditionals_Personal/js/conditionalsPersonal.js
+++ day6Conditionals/conditionals_Personal/js/conditionalsPersonal.js
@@ -5,3 +5,6 @@
  * Nov. 7, 2014
  */
 //alert("testing 1,2,3");
+
+//I am calculating which will be the best way to travel to my home in delaware.
+alert("We will be calculating the best way for you to travel home over vacation.");
