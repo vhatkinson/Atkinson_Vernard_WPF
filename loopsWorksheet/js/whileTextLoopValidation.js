f87a87fd7a1a43341d86b88ca74e7f11d6f15650
vhatkinson <vernard12345@GMail.com>
2014-11-11 16:43:09 -0500

    added if else to cookie
    do you want a cookie?

--- loopsWorksheet/js/whileTextLoopValidation.js
+++ loopsWorksheet/js/whileTextLoopValidation.js
@@ -8,3 +8,14 @@
 
 //we are going to ask user if they want a cookie.
 var answer=("Do you want a cookie?");
+
+//create a while for answering
+while(answer!="yes" && answer!="Yes" && answer!="no" && answer!="No"){
+    //re- prompt
+    answer=prompt("Please type in only yes or no:");
+    if (answer!="yes" && answer!="Yes"){
+        alert("Congratulations you want a cookie.");
+    }else{
+        alert("oh you don't want a cookie... cool story bro!");
+    }
+}console.log(answer);
