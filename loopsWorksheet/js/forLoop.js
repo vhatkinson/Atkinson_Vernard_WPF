81f5cbf9fa2554f34a926ed4d4b1907f2d06d663
vhatkinson <vernard12345@GMail.com>
2014-11-11 02:54:16 -0500

    made loop work.
    made it proper spelling too.

--- loopsWorksheet/js/forLoop.js
+++ loopsWorksheet/js/forLoop.js
@@ -8,4 +8,7 @@
 
 //using for loop
 //to teach kids to count from 0 to whatever number they are comfortable with learning up to.
-var final=prompt("What number do you want to learn to count to?");
+var final=prompt("We will help you learn how to count from 0.\nWhat number do you want to learn to count to?");
+for(var i=0;i<=final;i++) {
+    console.log("the number is "+i);
+}
