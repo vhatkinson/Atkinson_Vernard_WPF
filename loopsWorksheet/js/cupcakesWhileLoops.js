d52c2efaf907061d29bd0b63690bb6b7b47958dd
vhatkinson <vernard12345@GMail.com>
2014-11-10 22:46:01 -0500

    made loop for number of cupcakes
    while{
    else
    if}

--- loopsWorksheet/js/cupcakesWhileLoops.js
+++ loopsWorksheet/js/cupcakesWhileLoops.js
@@ -12,5 +12,13 @@
 var numberCupcakes=prompt("We will be finding out how many boxes of cupcake batter it will take to make a certain number of cupcakes!" +
 "\nHow many cupcakes would you like to make?");
 
+//creating while loop if they don't enter info or id doesn't make sense.
+while(numberCupcakes===""||isNaN(numberCupcakes)){
+    if(numberCupcakes===""){
+        numberCupcakes=prompt("How many cupcakes would you like to make? \nPlease do not leave blank.");
+    }else{
+        numberCupcakes=prompt("How many cupcakes would you like to make? \nPlease only type in numbers.");
+    }
+
 //number of cupcakes a box of batter makes
 var cupcakesPerBox=prompt("How many cupcakes does a box of cupcake batter make?");
