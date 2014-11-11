f3666907c2fee6d92c83fc0f7dc75575365abbfb
vhatkinson <vernard12345@GMail.com>
2014-11-11 01:51:50 -0500

    var for number to count up for
    prompt them to put it in.

--- loopsWorksheet/js/forLoop.js
+++ loopsWorksheet/js/forLoop.js
@@ -6,3 +6,6 @@
  */
 //alert("testing 1,2,3");
 
+//using for loop
+//to teach kids to count from 0 to whatever number they are comfortable with learning up to.
+var final=prompt("What number do you want to learn to count to?");
