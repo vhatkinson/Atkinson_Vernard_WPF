6846721a8bdd68b503f136526af812a9a05e0a83
vhatkinson <vernard12345@GMail.com>
2014-11-10 20:15:05 -0500

    make a while loop of zombies
    to a million woohoo

--- day 7/js/zombies.js
+++ day 7/js/zombies.js
@@ -14,17 +14,30 @@ var numZombies=1; // how many zombies we will have
 
 var numOfBites=4; //zombies can bite 4 a day
 
-var days=8; //number of days
+var days=1; //number of days //changed from 8
 
 //every day zombies will bite 4 people and turn them into zombies
 //loop number of days
 
-for(var i=1;i<=days;i++){
+/*for(var i=1;i<=days;i++){
 
     //how many new zombies get made and when do they go bite people.
     var newZombies=numOfBites*numZombies;
 
     //the bitten people become zombies
     numZombies=newZombies+numZombies; // can do numZombies+=newZombies
+
     console.log("there are now "+numZombies+" zombies on day #"+i+"!");
+}
+*/
+//how many days will it take it until you get 1 mil. zombies
+while(numZombies<=1000000){
+    //how many new zombies get made and when do they go bite people.
+    var newZombies=numOfBites*numZombies;
+
+    //the bitten people become zombies
+    numZombies=newZombies+numZombies; // can do numZombies+=newZombies
+
+    console.log("there are now "+numZombies+" zombies on day #"+days+"!");
+    days++;
 }
