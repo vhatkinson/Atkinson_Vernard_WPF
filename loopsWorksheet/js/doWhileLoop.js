ab6d00de6fef55794dca40004c40bfe8a8f2a87f
vhatkinson <vernard12345@GMail.com>
2014-11-11 00:01:51 -0500

    made do while loop for value 1
    do{}while

--- loopsWorksheet/js/doWhileLoop.js
+++ loopsWorksheet/js/doWhileLoop.js
@@ -6,4 +6,10 @@
  */
 //alert("testing 1,2,3");
 
+//alert telling user what we are doing
+alert("We are going to find the average of 2 numbers.");
+
 // creating do while loop to find the what the average of 2 numbers are.
+do{
+    var num1=prompt("What is your first number? \nPlease only type in numbers and don't leave blank.");
+}while(num1===""||isNaN(num1));
