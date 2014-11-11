3b0214926df5b2cc190be968253eaed93617d438
vhatkinson <vernard12345@GMail.com>
2014-11-10 22:33:57 -0500

    how many cupcakes you would like to make
    also told them what they are doing

--- loopsWorksheet/js/cupcakesWhileLoops.js
+++ loopsWorksheet/js/cupcakesWhileLoops.js
@@ -5,3 +5,9 @@
  * Nov. 10,2014
  */
 //alert("testing 1,2,3");
+
+//making a loop for making so many cupcakes if box only makes so many.
+
+//var for number of cupcakes you want to make
+var numberCupcakes=prompt("We will be finding out how many boxes of cupcake batter it will take to make a certain number of cupcakes!" +
+"\nHow many cupcakes would you like to make?");
