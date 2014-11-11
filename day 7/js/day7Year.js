7376043e89cb3216f26360c917be79e7565db003
vhatkinson <vernard12345@GMail.com>
2014-11-10 20:06:54 -0500

    end of zombies for loops
    number of zombies for amount of days

--- day 7/js/day7Year.js
+++ day 7/js/day7Year.js
@@ -22,9 +22,7 @@ for(var i=99;i>0;i--) {
     }else if(i===2){
         console.log(i+" bottles of beer on the wall. "+i+" bottles of beer.\n " +
         "you take one down and pass it around "+(i-1) +" bottle of beer on the wall.");
-    }else if(i==0){
+    }else if(i==00){
         break
     }
-
-
 }
--- day 7/js/zombies.js
+++ day 7/js/zombies.js
@@ -4,4 +4,27 @@
  * WPF 1411
  * Nov. 10, 2014
  */
-alert("testing 1,2,3");
+//alert("testing 1,2,3");
+//zombie attack at full sail.
+//it can bite 4 people a day and turn them into zombies by the next day.
+// the cdc has asked us how many zombies will there be in 8 days.
+
+//set up variables
+var numZombies=1; // how many zombies we will have
+
+var numOfBites=4; //zombies can bite 4 a day
+
+var days=8; //number of days
+
+//every day zombies will bite 4 people and turn them into zombies
+//loop number of days
+
+for(var i=1;i<=days;i++){
+
+    //how many new zombies get made and when do they go bite people.
+    var newZombies=numOfBites*numZombies;
+
+    //the bitten people become zombies
+    numZombies=newZombies+numZombies; // can do numZombies+=newZombies
+    console.log("there are now "+numZombies+" zombies on day #"+i+"!");
+}
