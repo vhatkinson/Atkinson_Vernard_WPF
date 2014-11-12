bcaecb25f66119f227174e97b00571f2c8b39799
vhatkinson <vernard12345@GMail.com>
2014-11-12 17:39:51 -0500

    created looping array files
    start

--- loopsWorksheet/js/whileTextLoopValidation.js
+++ loopsWorksheet/js/whileTextLoopValidation.js
@@ -1,19 +1,19 @@
 /**
  * Vernard Atkinson
- * title
+ * Cookie while text loop
  * WPF 1411
- * date
+ * Nov. 11, 2014
  */
 //alert("testing 1,2,3");
 
 //we are going to ask user if they want a cookie.
-var answer=("Do you want a cookie?");
+var answer=prompt("Do you want a cookie?");
 
 //create a while for answering
-while(answer!="yes" && answer!="Yes" && answer!="no" && answer!="No"){
+while((answer!="yes" || answer!="Yes") || (answer!="no" || answer!="No")){
     //re- prompt
     answer=prompt("Please type in only yes or no:");
-    if (answer!="yes" && answer!="Yes"){
+    if (answer==="yes" || answer==="Yes"){
         alert("Congratulations you want a cookie.");
     }else{
         alert("oh you don't want a cookie... cool story bro!");
