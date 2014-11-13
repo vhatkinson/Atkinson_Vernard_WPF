622798d1e4102074fb2d0b380919b85281dd7650
vhatkinson <vernard12345@GMail.com>
2014-11-12 22:24:07 -0500

    prompt for weight
    asked user to input weight

--- functionsWorksheet/js/stung.js
+++ functionsWorksheet/js/stung.js
@@ -5,3 +5,6 @@
  * Nov. 12, 2014
  */
 //alert("testing 1,2,3");
+
+//we will be calculating how may bee stings it would take to kill an animal. :(
+var weight=prompt("We will be calculating how many bee stings it would take to kill an animal.");
