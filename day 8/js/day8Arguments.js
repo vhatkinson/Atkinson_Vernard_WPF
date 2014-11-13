82c826eec757d98b1d3dd1edef13f528cc0b0bb4
vhatkinson <vernard12345@GMail.com>
2014-11-12 20:01:35 -0500

    finished arguments and parameters
    almost lunch break

--- day 8/js/day8Arguments.js
+++ day 8/js/day8Arguments.js
@@ -1,7 +1,39 @@
 /**
  * Vernard Atkinson
- * title
+ * Arguments and parameter
  * WPF 1411
- * date
+ * Nov. 12, 2014
  */
-alert("testing 1,2,3");
+//alert("testing 1,2,3");
+//argument goes in function-part of function call
+//parameter catches argument and is used inside of function
+
+//create function calculate the area of a rectangle-hard coded values-->not hard coded
+function calcArea(w,l){//parameters go here.
+    //var width=10;
+    //var length=20;
+    //var area=width*length;
+
+    var area=w*l;
+    console.log("area of rectangle is "+area);
+
+}
+//function call
+calcArea(4,5);//arguments go here
+calcArea(2,3);
+
+var width=prompt("what is the width");
+var length=prompt("what is the length");
+calcArea(width,length);
+
+
+//calculate dog years
+//dog years = human years*7
+function dogYears(humanAge){
+    var dogAge=humanAge*7;
+    console.log("you're "+dogAge+"years old in dog years");
+}
+//function call
+dogYears(18);
+var age=prompt("please type in your age");
+dogYears(age);
