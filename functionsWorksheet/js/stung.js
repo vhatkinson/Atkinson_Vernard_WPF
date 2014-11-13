49f22e6ed6d90480efae1fd5147746141005f0a3
vhatkinson <vernard12345@GMail.com>
2014-11-12 22:27:33 -0500

    created validation to prompt
    prompt was for user to enter weight

--- functionsWorksheet/js/stung.js
+++ functionsWorksheet/js/stung.js
@@ -7,4 +7,16 @@
 //alert("testing 1,2,3");
 
 //we will be calculating how may bee stings it would take to kill an animal. :(
-var weight=prompt("We will be calculating how many bee stings it would take to kill an animal.");
+var weight=prompt("We will be calculating how many bee stings it would take to kill an animal.\nHow much does the victim weigh in pounds?");
+
+
+
+//creating a validation to see if they input number or not
+function validation(w){
+    //create validation so if if it isn't a number to re prompt them.
+    while(w===""||isNaN(w)){
+        //re-prompt user
+        w=prompt("What is the radius of your circle?\nPlease type in only numbers. (do not leave blank.)");
+    }
+    return w;
+}
