4ff0bc302c01e4c928b10c6d24477b8ee96b73f9
vhatkinson <vernard12345@GMail.com>
2014-11-12 22:14:34 -0500

    function for calculating circumference
    i love this class

--- functionsWorksheet/js/circumference.js
+++ functionsWorksheet/js/circumference.js
@@ -10,3 +10,19 @@
 
 //what is the radius of the circle
 var radius=prompt("We will be calculating the circumference of a circle.\n What is the radius of your circle?");
+radius=validation(radius);
+
+//created function to calculate the circumference
+function circ(r){
+    return = (r*2*3.14)
+}
+
+//make validation for radius.
+function validation(num){
+    //create validation so if if it isn't a number to re prompt them.
+    while(num===""||isNaN(num)){
+        //re-prompt user
+        num=prompt("What is the radius of your circle?\nPlease type in only numbers. (do not leave blank.)");
+    }
+    return num;
+}
