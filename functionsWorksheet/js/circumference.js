ad271f70c6ea948f277d490b2bbe07264eabe838
vhatkinson <vernard12345@GMail.com>
2014-11-12 21:57:57 -0500

    asked for radius
    prompt

--- functionsWorksheet/js/circumference.js
+++ functionsWorksheet/js/circumference.js
@@ -7,3 +7,6 @@
 //alert("testing 1,2,3");
 
 //we are calculating the circumference of a circle
+
+//what is the radius of the circle
+var radius=prompt("We will be calculating the circumference of a circle.\n What is the radius of your circle?");
