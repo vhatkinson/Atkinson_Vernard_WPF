4d3f94cbd548428d20855888d261178abdb0d59a
vhatkinson <vernard12345@GMail.com>
2014-11-12 21:55:04 -0500

    added title and date
    proper formatting

---
+++ functionsWorksheet/js/circumference.js
@@ -0,0 +1,9 @@
+/**
+ * Vernard Atkinson
+ * circumference
+ * WPF 1411
+ * Nov. 12, 2014
+ */
+//alert("testing 1,2,3");
+
+//we are calculating the circumference of a circle
---
+++ functionsWorksheet/js/stung.js
@@ -0,0 +1,7 @@
+/**
+ * Vernard Atkinson
+ * Stung
+ * WPF 1411
+ * Nov. 12, 2014
+ */
+//alert("testing 1,2,3");
