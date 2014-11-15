34dd997d602a4d8b9ba6ff06da2898655ab593ac
vhatkinson <vernard12345@GMail.com>
2014-11-15 03:25:37 -0500

    validated circular numbers
    validated it.

--- functions/functionsIndustry/js/industryFunctions.js
+++ functions/functionsIndustry/js/industryFunctions.js
@@ -18,6 +18,12 @@ while(pool!="circular" && pool!="Circular" && pool!="rectangular" && pool!="Rect
     pool=prompt("Please type in only circular or rectangular for the shape of your pool!");
 }
 
+if (pool==="circular"||pool==="Circular"){
+    var r=prompt("What is the radius of your pool?");
+    var radius=validation(r);
+    var h=prompt("What is the height of the pool?");
+    var height=validation(h);
+}
 
 
 
@@ -33,4 +39,11 @@ function shape(pool){
         pool=prompt("please do not leave blank and only type in circular or rectangular");
     }
     return pool;
+}
+function validation(prompt){
+    while(prompt===""||isNaN(prompt)){
+        //re-prompt user
+        prompt=prompt("Please type in only numbers. (do not leave blank.)");
+    }
+    return prompt;
 }
